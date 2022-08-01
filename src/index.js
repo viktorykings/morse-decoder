7260886d0c3c44c5df5e77f4755fecbbfae7738e
viktorykings <kingsviktory@gmail.com>
2022-08-01 13:38:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,60 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let numberArr = expr.split('');
+    let tenStr = []
+
+    for (let i = 0; i <= numberArr.length; i= i+ 0.01){
+        let splisedArr = numberArr.splice(0, 10) ;
+        tenStr.push(splisedArr.join(''))
+       
+    }
+    // tenStr.push(numberArr.slice(0,10).join(''))
+
+  
+
+    const space = '**********'
+    
+// console.log(numberArr);
+
+    const morseArr = []
+    for( let i = 0; i < tenStr.length; i++){
+        morseArr.push(tenStr[i].replaceAll('10', '.').replaceAll('11', '-').replaceAll(' ', space).replaceAll('0', ' ').trim()) 
+    }
+ 
+
+    const keysArr = Object.keys(MORSE_TABLE);
+    const valuesArr = Object.values(MORSE_TABLE);
+
+
+//     for( let i = 0; i <= morseArr.length; i++){
+//         // if (morseArr[i] === space){ 
+//         //     letterArr.push(' ')
+//         // } else 
+//         if (morseArr[i] !== space){
+//             for( let j = 0; j <= morseArr.length; j++){
+
+//             if (morseArr[i] === keysArr[j]){
+//                 letterArr.push(valuesArr[j])
+//             }
+//         } 
+//     } else { letterArr.push(' ')}
+// }
+
+const letterArr = []
+morseArr.forEach(function (el) {
+ if (el === space){
+    letterArr.push(' ')
+ }
+ for (let i = 0; i <= keysArr.length; i++){
+    if (el === keysArr[i]){
+        letterArr.push(valuesArr[i])
+    } 
+ }
+})
+
+return letterArr.join('')
 }
 
 module.exports = {
